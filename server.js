2e89b28293cf3f90df56ad827fe3e26aae561014
vrindashine1 <vrindashine2507@gmail.com>
2024-07-18 22:08:00 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,100 @@
+// import express from "express";
+// import colors from "colors";
+// import dotenv from "dotenv";
+// import morgan from "morgan";
+// import connectDB from "./config/db.js";
+// import authRoutes from "./routes/authRoute.js";
+// import categoryRoutes from "./routes/categoryRoutes.js";
+// import productRoutes from "./routes/productRoutes.js";
+// import cors from "cors";
+// import path from "path";
+// //configure env
+// dotenv.config();
+
+// //database config
+// connectDB();
+// //rest object
+// const app = express();
+
+// //middlewares
+// app.use(cors());
+// app.use(express.json());
+// app.use(morgan("dev"));
+// app.use(express.static(path.join(__dirname, "./client/build")));
+// //routes
+// app.use("/api/v1/auth", authRoutes);
+// app.use("/api/v1/category", categoryRoutes);
+// app.use("/api/v1/product", productRoutes);
+
+// //rest api
+// app.use("*", function (req, res) {
+//   res.sendFile(path.join(__dirname, "./client/build/index.html"));
+// });
+
+// //PORT
+// const PORT = process.env.PORT || 8080;
+
+// //run listen
+// app.listen(PORT, () => {
+//   console.log(
+//     `server running on ${process.env.DEV_MODE} mode on port ${PORT}`.bgCyan
+//       .white
+//   );
+// });
+import express from "express";
+import colors from "colors";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoute.js";
+import categoryRoutes from "./routes/categoryRoutes.js";
+import productRoutes from "./routes/productRoutes.js";
+import cors from "cors";
+
+import path from "path";
+import { fileURLToPath } from 'url';
+
+//configure env
+dotenv.config();
+
+//database config
+connectDB();
+//rest object
+const app = express();
+
+//middlewares
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
+
+//routes
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/category", categoryRoutes);
+app.use("/api/v1/product", productRoutes);
+//rest api
+app.get("/", (req, res) => {
+  res.send("<h1>welcome to ecommerce app</h1>");
+});
+
+//
+// Serve static files from the React app
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+// The "catchall" handler: for any request that doesn't match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client/build/index.html'));
+});
+
+//
+//PORT
+const PORT = process.env.PORT || 8080;
+
+//run listen
+app.listen(PORT, () => {
+  console.log(
+    `server running on ${process.env.DEV_MODE} mode on port ${PORT}`.bgCyan
+      .white
+  );
+});
